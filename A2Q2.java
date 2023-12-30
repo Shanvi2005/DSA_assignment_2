d5cc1ddd489d94816b47b2868d0fb1579db8f509
Shanvi2005 <144417466+Shanvi2005@users.noreply.github.com>
2023-12-30 17:38:47 +0530

    Create A2Q2.java

---
+++ A2Q2.java
@@ -0,0 +1,18 @@
+package Solutions;
+import java.util.*;
+public class A2Q2
+{
+	public static void main(String[]args)
+	{
+		Scanner scn = new Scanner(System.in);
+		int n= scn.nextInt();
+		for (int i =1;i<=n;i++)
+		{
+			for(int j =1; j<=(n+1-i);j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+	}
+}
