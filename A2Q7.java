3c7904934130739a9b46529361d1c4de98f1df69
Shanvi2005 <144417466+Shanvi2005@users.noreply.github.com>
2023-12-30 17:42:19 +0530

    Create A2Q7.java

---
+++ A2Q7.java
@@ -0,0 +1,19 @@
+package Solutions;
+import java.util.*;
+public class A2Q7
+{
+	public static void main(String[]args)
+	{
+		Scanner scn = new Scanner(System.in);
+		int n= scn.nextInt();
+		for(int i=1;i<=n;i++)
+		{
+			for(int j =1; j<=i-1;j++)
+			{
+				System.out.print(" ");
+			}
+			System.out.print("*");
+			System.out.println();
+		}
+	}
+}
